2ceac131f8ed28986aae79e3e4959282b4a5cdb8
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-05-01 12:20:29 +0500

    fixing bugs in redux setup

--- src/Store/Provider.tsx
+++ src/Store/Provider.tsx
@@ -3,9 +3,12 @@
 import { store } from './store'
 import { Provider } from 'react-redux'
 
+type ProvidersProps = {
+  children: React.ReactNode
+}
 
 
-export function Providers ({ children }) {
+export function Providers ({ children } : ProvidersProps) {
   return (
     <Provider store={store}>
       {children}
